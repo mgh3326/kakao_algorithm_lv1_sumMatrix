481ff485d98dc28800df97a26dd35d2b6a0bcbe2
mgh3326 <mgh3326@naver.com>
2017-11-09 23:13:39 +0900

    complete

--- kakao_algorithm_lv1_sumMatrix/sumMatrix.cpp
+++ kakao_algorithm_lv1_sumMatrix/sumMatrix.cpp
@@ -5,14 +5,19 @@ using namespace std;
 vector<vector<int> > sumMatrix(vector<vector<int> >A, vector<vector<int> >B)
 {
 	vector<vector<int> > answer;
-
+	for(int i=0;i<A.size();i++)
+		for (int j = 0; j < A[0].size(); j++)
+		{
+			A[i][j] += B[i][j];
+		}
+	answer = A;
 	return answer;
 }
 int main()
 {
-	vector<vector<int> > a{ { 1,2 },{ 2,3 } }, b{ { 3,4 },{ 5,6 } };
+	vector<vector<int> > a{ { 1,2 },{ 2,3 } }, b{ { 3,4 },{ 5,6 } };//벡터의 벡터도 가능하구나
 	vector<vector<int> > answer = sumMatrix(a, b);
-
+	
 	for (int i = 0; i<answer.size(); i++)
 	{
 		for (int j = 0; j<answer[0].size(); j++)
@@ -22,3 +27,10 @@ int main()
 		cout << "\n";
 	}
 }
+/*
+행렬의 덧셈은 행과 열의 크기가 같은 두 행렬의 같은 행, 같은 열의 값을 서로 더한 결과가 됩니다.
+2개의 행렬을 입력받는 sumMatrix 함수를 완성하여 행렬 덧셈의 결과를 반환해 주세요.
+
+예를 들어 2x2 행렬인 A = ((1, 2), (2, 3)), B = ((3, 4), (5, 6)) 가 주어지면, 같은 2x2 행렬인 ((4, 6), (7, 9))를 반환하면 됩니다.
+(어떠한 행렬에도 대응하는 함수를 완성해주세요.)
+*/
